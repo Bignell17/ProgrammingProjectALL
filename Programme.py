ef064bf35552c4fe1da4d5a3c2b88841acacad7a
Joseph Bundy <joebundy1@yahoo.co.uk>
2014-12-10 17:10:06 +0000

    API

--- Programme.py
+++ Programme.py
@@ -4,9 +4,13 @@ url = "http://dev.c0l.in:9999/"
 response = urllib2.urlopen(url).read()
 data = json.loads(response.decode('utf8'))
 
-Listofsectors = ["financial", "industrial goods", "healthcare", "services", "basic materials", "technology", "consumer goods", "utilities"]
+listofsectors = ["financial", "industrial goods", "healthcare", "services", "basic materials", "technology", "consumer goods", "utilities"]
 
 user_input = raw_input("Choose a sector:\n")
+while user_input != listofsectors:
+    user_input = raw_input("Choose a sector:\n")
+else:
+    print "found"
 
 
 
