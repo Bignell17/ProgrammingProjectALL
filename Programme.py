c26993f12041b04c6b75814c99f59664be151066
Joseph Bundy <joebundy1@yahoo.co.uk>
2014-12-10 12:42:58 +0000

    programme

---
+++ Programme.py
@@ -0,0 +1,15 @@
+import urllib2
+import json
+url = "http://dev.c0l.in:8888/"
+response = urllib2.urlopen(url).read()
+data = json.loads(response.decode('utf8'))
+
+Listofsectors = ["financial", "industrial goods", "healthcare", "services", "basic materials", "technology", "consumer goods", "utilities"]
+
+user_input = raw_input("Choose a sector")
+
+for word in Listofsectors:
+    if user_input == word:
+        found = true
+        print "found"
+        
