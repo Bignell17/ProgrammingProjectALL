be485b3ac8aab2eeaabca3b7fccd6b1e31c04b44
Joseph Bundy <joebundy1@yahoo.co.uk>
2015-01-22 20:16:42 +0000

    API

--- Api attempt.py
+++ Api attempt.py
@@ -6,3 +6,18 @@ finpos = csv.writer(open("financial position output.csv", "wb"))
 
 incomestatement = csv.writer(open("income position output.csv", "wb"))
 #this opens the income statement output file and makes that wb
+
+def income_statement():
+    url = "http://dev.c0l.in:8888/"
+    response = urllib2.urlopen(url).read()
+    data = json.loads(response.decode('utf8'))
+    #decodes the url data into python
+    incomestatement.writerow([
+        'Name',
+        'Sales',
+        'Interest Payable',
+        'Expenses',
+        'Interest Recivable',
+        'Opening Stock',
+        'Closing Stock'])
+    #names the columns in the excel document
